21036e7ee12a46a668d1f365fb59b5fa865c8c15
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-09-20 18:37:54 -0300

    refactor next future image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { AppProps } from 'next/app'
-import { globalStyles } from '../styles/global'
+import Image from 'next/future/image'
 
 import logoImg from '../assets/logo.svg'
-import Image from 'next/image'
 import { Container, Header } from '../styles/pages/app'
+
+import { globalStyles } from '../styles/global'
 
 globalStyles()
 
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Image src={logoImg} alt="" priority />
       </Header>
       <Component {...pageProps} />
     </Container>
