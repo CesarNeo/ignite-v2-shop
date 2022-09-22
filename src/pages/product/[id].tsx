8af3cc259c35ef20e9cd31999b68dde88c05f73e
Cesar Emmanuel <cesaremmmanul@gmail.com>
2022-09-22 18:47:46 -0300

    feat: product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,31 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
   console.log(query)
 
-  return <h1>Product</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta x1</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          minus incidunt atque enim, totam nulla sed earum alias qui molestiae,
+          dolor expedita iusto consequuntur. Omnis dicta facilis blanditiis
+          deserunt perferendis!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
